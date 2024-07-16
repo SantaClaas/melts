ba40c7b54ec97a2072a774adc7193dbb0da6a527
Claas <claas@claas.dev>
2024-07-16 15:43:00 +0200

    Remove wasm-bindgen because web projects were removed

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -2,7 +2,6 @@ pub mod app;
 pub mod database;
 
 use std::sync::OnceLock;
-use wasm_bindgen::prelude::wasm_bindgen;
 
 pub use crux_core::{bridge::Bridge, Core, Request};
 
@@ -16,17 +15,14 @@ fn core() -> &'static Bridge<Effect, Counter> {
     CORE.get_or_init(|| Bridge::new(Core::new()))
 }
 
-#[wasm_bindgen]
 pub fn process_event(data: &[u8]) -> Vec<u8> {
     core().process_event(data)
 }
 
-#[wasm_bindgen]
 pub fn handle_response(id: u32, data: &[u8]) -> Vec<u8> {
     core().handle_response(id, data)
 }
 
-#[wasm_bindgen]
 pub fn view() -> Vec<u8> {
     core().view()
 }
